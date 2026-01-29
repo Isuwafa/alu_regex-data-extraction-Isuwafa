4a67aa5708c676b7f3f93e2163ce0f23391e0ff9
Suwafa <s.iradukund2@alustudent.com>
2026-01-29 12:14:53 -0800

    my sample_input.txt file

---
+++ Extractor.js
@@ -0,0 +1,7 @@
+/**
+ * extractor.js
+ * 
+ * This script extracts structured data from raw text using regular expressions.
+ * It demonstrates validation of untrusted input and basic security practices
+ * such as rejecting malformed data and masking sensitive information.
+ */
