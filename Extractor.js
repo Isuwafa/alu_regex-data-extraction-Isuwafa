eb0fbcd2dfe6181c9712283ccb6c330e4ea3d8d2
Suwafa <s.iradukund2@alustudent.com>
2026-01-29 16:29:53 -0800

    Added new comprehensive sample input text

--- Extractor.js
+++ Extractor.js
@@ -8,10 +8,9 @@
 const fs = require('fs');
 
 // 1. Read input text from file
-const rawText = fs.readFileSync("sample_input.txt", "utf-8");
+const rawText = fs.readFileSync("./sample_input.txt", "utf-8");
 
 console.log("=== RAW INPUT TEXT ===");
-console.log(rawText);
 
 // 2. Placeholder arrays for extracted datagit 
 const emails = [];
@@ -23,4 +22,22 @@ const creditCards = [];
 const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
 const urlRegex = /https?:\/\/[^\s/$.?#].[^\s]*/g;
 const phoneRegex = /\b\d{3}[-.]?\d{3}[-.]?\d{4}\b/g;
-const creditCardRegex = /\b(?:\d[ -]*?){13,16}\b/g; 
+const creditCardRegex = /\b(?:\d[ -]*?){13,16}\b/g;
+
+console.log(emailRegex.test(rawText));
+console.log(rawText);
+
+// # 4. Extract and validate data
+let match;
+while ((match = emailRegex.exec(rawText)) !== null) {
+    emails.push(match[0]);
+}
+while ((match = urlRegex.exec(rawText)) !== null) {
+    urls.push(match[0]);
+}
+while ((match = phoneRegex.exec(rawText)) !== null) {
+    phoneNumbers.push(match[0]);
+}
+while ((match = creditCardRegex.exec(rawText)) !== null) {
+    creditCards.push(match[0]);
+}
